7bdc367f831161568c04ec79058a6fc7b3f796fd
Sevriukoff <igor.sevriukoff@yandex.ru>
2024-02-23 05:21:01 +0700

    Added a specification for filtering projects

---
+++ Sevriukoff.ProjectManager.Application/Specification/Project/PrioritySpecification.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq.Expressions;
+using Sevriukoff.ProjectManager.Infrastructure.Base;
+
+namespace Sevriukoff.ProjectManager.Application.Specification.Project;
+
+public class PrioritySpecification : Specification<Infrastructure.Entities.Project>
+{
+    private readonly int? _priority;
+
+    public PrioritySpecification(int? priority)
+    {
+        _priority = priority;
+    }
+
+    public override Expression<Func<Infrastructure.Entities.Project, bool>> ToExpression()
+    {
+        return p => !_priority.HasValue || p.Priority == _priority;
+    }
+}
---
+++ Sevriukoff.ProjectManager.Application/Specification/Project/StartDateSpecification.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq.Expressions;
+using Sevriukoff.ProjectManager.Infrastructure.Base;
+
+namespace Sevriukoff.ProjectManager.Application.Specification.Project;
+
+public class StartDateSpecification : Specification<Infrastructure.Entities.Project>
+{
+    private readonly DateTime _startDateFrom;
+    private readonly DateTime _startDateTo;
+
+    public StartDateSpecification(DateTime startDateFrom, DateTime startDateTo)
+    {
+        _startDateFrom = startDateFrom;
+        _startDateTo = startDateTo;
+    }
+
+    public override Expression<Func<Infrastructure.Entities.Project, bool>> ToExpression()
+    {
+        return p => p.StartDate >= _startDateFrom && p.StartDate <= _startDateTo;
+    }
+}
