0909c0fe3c06dcdece3776865a5393b444635679
Sevriukoff <igor.sevriukoff@yandex.ru>
2024-02-23 05:19:44 +0700

    Added a base implementation of specification pattern

---
+++ Sevriukoff.ProjectManager.Infrastructure/Base/CompositeSpecification.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq.Expressions;
+
+namespace Sevriukoff.ProjectManager.Infrastructure.Base;
+
+public class CompositeSpecification<T> : Specification<T>
+{
+    private readonly Specification<T> _left;
+    private readonly Specification<T> _right;
+
+    public CompositeSpecification(Specification<T> left, Specification<T> right)
+    {
+        _left = left;
+        _right = right;
+        
+        var leftExpression = _left.ToExpression();
+        var rightExpression = _right.ToExpression();
+
+        var paramExpr = System.Linq.Expressions.Expression.Parameter(typeof(T));
+        var exprBody = System.Linq.Expressions.Expression.AndAlso(leftExpression.Body, rightExpression.Body);
+        exprBody = (BinaryExpression)new ParameterReplacer(paramExpr).Visit(exprBody);
+        
+        Expression = System.Linq.Expressions.Expression.Lambda<Func<T, bool>>(exprBody, paramExpr);
+    }
+    
+    public override Expression<Func<T, bool>> ToExpression()
+    {
+        return Expression;
+    }
+}
+
+public class ParameterReplacer : ExpressionVisitor
+{
+    private readonly ParameterExpression _parameter;
+
+    protected override Expression VisitParameter(ParameterExpression node)
+        => base.VisitParameter(_parameter);
+
+    internal ParameterReplacer(ParameterExpression parameter)
+    {
+        _parameter = parameter;
+    }
+}
---
+++ Sevriukoff.ProjectManager.Infrastructure/Base/ISpecification.cs
@@ -0,0 +1,6 @@
+﻿namespace Sevriukoff.ProjectManager.Infrastructure.Base;
+
+public interface ISpecification<T>
+{
+    bool IsSatisfiedBy(T entity);
+}
