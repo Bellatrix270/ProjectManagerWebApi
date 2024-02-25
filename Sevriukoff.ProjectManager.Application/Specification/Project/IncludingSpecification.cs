03b8c4748d18e8cc89def785aa2f1ebf6ec5fae8
Sevriukoff <igor.sevriukoff@yandex.ru>
2024-02-25 17:00:18 +0700

    Added a generic specification pattern to sorting and inclusion of properties

---
+++ Sevriukoff.ProjectManager.Application/Specification/Project/IncludingSpecification.cs
@@ -0,0 +1,10 @@
+﻿namespace Sevriukoff.ProjectManager.Application.Specification.Project;
+
+public class IncludingSpecification<T> : Specification<T>
+{
+    public IncludingSpecification(params string[] includes)
+    {
+        foreach (var include in includes)
+            AddInclude(include);
+    }
+}
