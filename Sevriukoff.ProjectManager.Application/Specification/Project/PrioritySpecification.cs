7fc23fc1423e687f854157b3268b609b8362ebc1
Sevriukoff <igor.sevriukoff@yandex.ru>
2024-02-25 17:01:52 +0700

    Added specifications for project entity

--- Sevriukoff.ProjectManager.Application/Specification/Project/PrioritySpecification.cs
+++ Sevriukoff.ProjectManager.Application/Specification/Project/PrioritySpecification.cs
@@ -1,19 +1,10 @@
-﻿using System.Linq.Expressions;
-using Sevriukoff.ProjectManager.Infrastructure.Base;
-
-namespace Sevriukoff.ProjectManager.Application.Specification.Project;
+﻿namespace Sevriukoff.ProjectManager.Application.Specification.Project;
 
 public class PrioritySpecification : Specification<Infrastructure.Entities.Project>
 {
-    private readonly int? _priority;
-
     public PrioritySpecification(int? priority)
     {
-        _priority = priority;
-    }
-
-    public override Expression<Func<Infrastructure.Entities.Project, bool>> ToExpression()
-    {
-        return p => !_priority.HasValue || p.Priority == _priority;
+        if (priority.HasValue)
+            SetFilterCondition(p => p.Priority == priority);
     }
 }
---
+++ Sevriukoff.ProjectManager.Application/Specification/Project/TimePeriodSpecification.cs
@@ -0,0 +1,14 @@
+﻿namespace Sevriukoff.ProjectManager.Application.Specification.Project;
+
+public class TimePeriodSpecification : Specification<Infrastructure.Entities.Project>
+{
+    public TimePeriodSpecification(DateTime? startDateFrom, DateTime? startDateTo)
+    {
+        if (!startDateFrom.HasValue) return;
+
+        if (startDateTo.HasValue)
+            SetFilterCondition(p => p.StartDate >= startDateFrom && p.StartDate <= startDateTo);
+        else
+            SetFilterCondition(p => p.StartDate >= startDateFrom);
+    }
+}
